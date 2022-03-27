b8660e32fcea7fd9eee7a5e17559368e6910a77d
vasilich99 <oleg_kovtkov@mail.ru>
2022-03-28 02:16:18 +0300

    fiiiix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Index from './pages';
 import Test from './pages/test/[part]';
 
 function App() {
   return (
-    <Router basename="/english">
-      <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='/test/:part' element={<Test />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={<Index />} />
+      <Route path='/test/:part' element={<Test />} />
+    </Routes>
   );
 }
 
