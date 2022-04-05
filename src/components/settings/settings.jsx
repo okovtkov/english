b8821db53b8663b76d23cf332ad0caa92b749df6
vasilich99 <oleg_kovtkov@mail.ru>
2022-04-05 23:20:39 +0300

    fix: styles. And add some changes to parts & settings components

--- src/components/settings/settings.jsx
+++ src/components/settings/settings.jsx
@@ -10,7 +10,7 @@ import './settings.scss';
 function Settings(props) {
   const [currentElement, setCurrentElement] = useState(null);
   const [currentId, setCurrentId] = useState('');
-  const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(props.data.length === 0 ? true : false);
   const [popup, setPopup] = useState(false);
 
   const onClick = useCallback((item, e) => {
@@ -40,7 +40,9 @@ function Settings(props) {
     <>
       <div className="settings">
         <div className="settings__parts">
-          {console.log(props.data)}
+          <p className={classNames("settings__void", {
+            "settings__void--active": props.data.length === 0 && editing === false,
+          })}>У вас пока что нет разделов со словами.</p>
           {props.data.map((item) => (
             <div key={item.id} className={classNames("settings__wrapper", {
               "settings__wrapper--editing": editing,
