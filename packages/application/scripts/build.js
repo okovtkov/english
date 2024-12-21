5b0efcfe1e46aa885761e8584f0fb0aae35d08e8
Oleh Kovtkov <okovtkov@gmail.com>
2024-12-21 01:57:59 +0100

    проверка

--- packages/application/scripts/build.js
+++ packages/application/scripts/build.js
@@ -96,7 +96,7 @@ checkBrowsers(paths.appPath, isInteractive)
 
       const appPackage = require(paths.appPackageJson);
       const publicUrl = paths.publicUrlOrPath;
-      const publicPath = config.output.publicPath;
+      const publicPath = `${config.output.publicPath}/chore/37-packages/`;
       const buildFolder = path.relative(process.cwd(), paths.appBuild);
       printHostingInstructions(
         appPackage,
