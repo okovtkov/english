44b2a3c7265afd1764de191c89058c8230a5783b
Oleh Kovtkov <okovtkov@gmail.com>
2025-10-19 00:03:16 +0200

    fix: выбор голосов

--- packages/api/src/audio.js
+++ packages/api/src/audio.js
@@ -3,7 +3,8 @@ export const audio = {
     const voices = speechSynthesis.getVoices();
     const rus = new SpeechSynthesisUtterance('');
     rus.lang = 'ru-RU';
-    rus.voice = voices[17];
+    const voice = voices.find((voice) => voice.name === 'Google русский');
+    rus.voice = voice;
     speechSynthesis.speak(rus);
     speechSynthesis.cancel();
   },
@@ -32,12 +33,16 @@ export const audio = {
       const voices = speechSynthesis.getVoices();
       const rus = new SpeechSynthesisUtterance(word);
       rus.lang = 'ru-RU';
-      rus.voice = voices[17];
+      const voice = voices.find((voice) => voice.name === 'Google русский');
+      rus.voice = voice;
       speechSynthesis.speak(rus);
       return rus;
     } else {
+      const voices = speechSynthesis.getVoices();
       const eng = new SpeechSynthesisUtterance(word);
       eng.lang = 'en-US';
+      const voice = voices.find((voice) => voice.name === 'Google US English');
+      eng.voice = voice;
       eng.rate = 0.7;
       speechSynthesis.speak(eng);
       return eng;
